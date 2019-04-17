9abfadda78d2aebb66c647d2aa1d94491301abe7
Siegfried De Bleeckere <siegfried.debleeckere@ipcos.com>
2019-04-17 22:26:47 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bf7b4e2a4fa904090805.js"
+    "url": "webpack-runtime-d274ee527ccdea480f7f.js"
   },
   {
     "url": "app-a75c8ddd52ffed06315c.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2a2e2c763686c5b5796e5eaf9153453c"
+    "revision": "e1818a45005f77c79e6c1b356c47aaa7"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
