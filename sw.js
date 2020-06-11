23797c2471813f2f26269198af6eb8099d9440d5
Siegfried De Bleeckere <Siegfried.DeBleeckere@ipcos.com>
2020-06-12 04:29:26 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,17 +26,17 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1223c36a8c5c27f79402.js"
+    "url": "webpack-runtime-823dfca2a5469291a5ca.js"
   },
   {
-    "url": "app-b9280d095f5e39501a93.js"
+    "url": "app-8170d6ccf624a80faeef.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-323a5671eba2a2a455ce.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "89fa0357419d1e95bd104f9ebdc5572e"
+    "revision": "08b877605dad2c5778f1642de78b5f1e"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
