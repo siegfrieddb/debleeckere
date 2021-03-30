7f57a08a3c3911dcd741df31152f179bfd01e3ca
sylvain <sylvain@mentis.be>
2021-03-30 21:56:44 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0d301dd8ebc2fa457421.js"
+    "url": "webpack-runtime-6d7c7f90ff4f9b0b1672.js"
   },
   {
     "url": "app-4f16df8037ef3e5f3712.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e1ad142a6e3804277e79da22469a88cf"
+    "revision": "6fc247e3231cdea2ec77963cb66a992b"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
